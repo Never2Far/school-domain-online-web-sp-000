64ba08fff694bd50e2671023e85cc7cf464e8c51
Seth Near <seth.j.near@gmail.com>
2020-06-22 14:13:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,8 @@ def add_student(name, grade)
   if @roster.keys.include?(grade)
     @roster[grade] << name
   else
-    @roster[grade] = []
-    @roster[grade] << name
+    @roster[grade:] = []
+    @roster[grade:] << name
   end
 end
 
